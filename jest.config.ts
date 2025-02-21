3c43fa673d0fdfbc6f7e94a638a890d2340c8343
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-21 15:54:02 +0100

    chore (*): updated project configuration

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type {Config} from 'jest';
+
+export default (): Config => ({
+    extensionsToTreatAsEsm: ['.ts'],
+    collectCoverage: process.argv.includes('--coverage'),
+    collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/**/index.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        }
+    },
+    transform: {
+        '^.+\\.ts$': 'ts-jest'
+    },
+    moduleFileExtensions: ['ts', 'js'],
+    rootDir: './src',
+    testEnvironment: 'node',
+    verbose: true,
+});

bb49f894afa139edbad76b87d08bb4cda8f0a1c7
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-13 16:29:49 +0100

    feature (*): uuid v4 generator

---
+++ src/other/uuid.ts
@@ -0,0 +1,28 @@
+/**
+ * Creates an UUID v4 "unique" string.
+ * @see https://en.wikipedia.org/wiki/Universally_unique_identifier
+ * @return  {String}    "unique" string
+ */
+export const uuid = (): string =>
+{
+    let seed = Date.now();
+    try
+    {
+        if (window && window.performance && window.performance.now instanceof Function)
+        {
+            seed += window.performance.now();
+        }
+    }
+    catch (e: unknown)
+    {
+        console.error(`[@amjs/js-utils][ERROR] %o`, e);
+    }
+
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c =>
+    {
+        const r = (seed + Math.random() * 16) % 16 | 0;
+        seed = Math.floor(seed / 16);
+
+        return (c === 'x' ? r : r & (0x3 | 0x8)).toString(16);
+    });
+};

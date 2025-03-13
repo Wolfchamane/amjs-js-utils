fb16ff5633db539df1357eaaebe094d59f8eab1b
Wolfchamane <arturo.martinez@amartinez.dev>
2025-03-13 16:32:19 +0100

    fix (*): format

--- src/other/uuid.ts
+++ src/other/uuid.ts
@@ -3,23 +3,17 @@
  * @see https://en.wikipedia.org/wiki/Universally_unique_identifier
  * @return  {String}    "unique" string
  */
-export const uuid = (): string =>
-{
+export const uuid = (): string => {
     let seed = Date.now();
-    try
-    {
-        if (window && window.performance && window.performance.now instanceof Function)
-        {
+    try {
+        if (window && window.performance && window.performance.now instanceof Function) {
             seed += window.performance.now();
         }
-    }
-    catch (e: unknown)
-    {
+    } catch (e: unknown) {
         console.error(`[@amjs/js-utils][ERROR] %o`, e);
     }
 
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c =>
-    {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
         const r = (seed + Math.random() * 16) % 16 | 0;
         seed = Math.floor(seed / 16);
 
